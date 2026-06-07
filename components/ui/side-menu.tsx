685c705dbdc0cca2efbf47b0cfe1e7f6ddd0e183
Провальская Дарья <provalskayadaria@gmail.com>
2026-06-07 20:53:57 +0300

    кнопки авторизации

--- components/ui/side-menu.tsx
+++ components/ui/side-menu.tsx
@@ -1,5 +1,6 @@
+import { logout } from "@/store/slices";
 import { isAuthorized } from "@/utils";
-import { Link } from "expo-router";
+import { Link, useRouter } from "expo-router";
 import { useEffect, useRef } from "react";
 import {
   Animated,
@@ -10,16 +11,109 @@ import {
   TouchableOpacity,
   View,
 } from "react-native";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Button } from "./button";
 
 interface SideMenuProps {
   onClose: () => void;
 }
 
+// export const SideMenu = ({ onClose }: SideMenuProps) => {
+//   const slideAnim = useRef(new Animated.Value(300)).current;
+//   const fadeAnim = useRef(new Animated.Value(0)).current;
+
+//   const currentUser = useSelector((state: any) => state.user.user);
+//   const isAuth = isAuthorized(currentUser?.id);
+
+//   useEffect(() => {
+//     Animated.parallel([
+//       Animated.timing(slideAnim, {
+//         toValue: 0,
+//         duration: 250,
+//         useNativeDriver: true,
+//       }),
+//       Animated.timing(fadeAnim, {
+//         toValue: 1,
+//         duration: 250,
+//         useNativeDriver: true,
+//       }),
+//     ]).start();
+//   }, []);
+
+//   const closeMenu = () => {
+//     Animated.parallel([
+//       Animated.timing(slideAnim, {
+//         toValue: 300,
+//         duration: 250,
+//         useNativeDriver: true,
+//       }),
+//       Animated.timing(fadeAnim, {
+//         toValue: 0,
+//         duration: 250,
+//         useNativeDriver: true,
+//       }),
+//     ]).start(() => onClose());
+//   };
+
+//   const profileLink = isAuth ? "/profile" : "/auth/login";
+
+//   return (
+//     <Animated.View style={[styles.overlay, { opacity: fadeAnim }]}>
+//       <Pressable style={StyleSheet.absoluteFill} onPress={closeMenu} />
+//       <Animated.View
+//         style={[
+//           styles.menu,
+//           {
+//             transform: [{ translateX: slideAnim }],
+//           },
+//         ]}
+//       >
+//         <TouchableOpacity style={styles.closeButton} onPress={closeMenu}>
+//           <Image
+//             source={require("../../public/img/burger-menu-2.png")}
+//             style={{ width: 20, height: 10 }}
+//           />
+//         </TouchableOpacity>
+
+//         {/* TODO: мь вынести в компонент */}
+//         <View style={styles.nav}>
+//           <Link href="/" asChild>
+//             <Text style={styles.navItem}>Главная</Text>
+//           </Link>
+
+//           <Link href="/events" asChild>
+//             <Text style={styles.navItem}>Мероприятия</Text>
+//           </Link>
+
+//           <Link href={profileLink} asChild>
+//             <Text style={styles.navItem}>Профиль</Text>
+//           </Link>
+//         </View>
+
+//         <View style={styles.buttons}>
+//           <Link href="/auth/register" asChild>
+//             <Button backgroundColor="#C0A2E2" width={170}>
+//               Зарегистрироваться
+//             </Button>
+//           </Link>
+
+//           <Link href="/auth/login" asChild>
+//             <Button border="2px solid #C0A2E2" width={150}>
+//               Войти в аккаунт
+//             </Button>
+//           </Link>
+//         </View>
+//       </Animated.View>
+//     </Animated.View>
+//   );
+// };
+
 export const SideMenu = ({ onClose }: SideMenuProps) => {
   const slideAnim = useRef(new Animated.Value(300)).current;
   const fadeAnim = useRef(new Animated.Value(0)).current;
+
+  const dispatch = useDispatch();
+  const router = useRouter();
 
   const currentUser = useSelector((state: any) => state.user.user);
   const isAuth = isAuthorized(currentUser?.id);
@@ -56,6 +150,12 @@ export const SideMenu = ({ onClose }: SideMenuProps) => {
 
   const profileLink = isAuth ? "/profile" : "/auth/login";
 
+  const handleLogout = () => {
+    dispatch(logout());
+    closeMenu();
+    router.replace("/auth/login");
+  };
+
   return (
     <Animated.View style={[styles.overlay, { opacity: fadeAnim }]}>
       <Pressable style={StyleSheet.absoluteFill} onPress={closeMenu} />
@@ -74,7 +174,6 @@ export const SideMenu = ({ onClose }: SideMenuProps) => {
           />
         </TouchableOpacity>
 
-        {/* TODO: мь вынести в компонент */}
         <View style={styles.nav}>
           <Link href="/" asChild>
             <Text style={styles.navItem}>Главная</Text>
@@ -90,17 +189,37 @@ export const SideMenu = ({ onClose }: SideMenuProps) => {
         </View>
 
         <View style={styles.buttons}>
-          <Link href="/auth/register" asChild>
-            <Button backgroundColor="#C0A2E2" width={170}>
-              Зарегистрироваться
-            </Button>
-          </Link>
+          {!isAuth ? (
+            <>
+              <Link href="/auth/register" asChild>
+                <Button backgroundColor="#C0A2E2" width={170}>
+                  Зарегистрироваться
+                </Button>
+              </Link>
 
-          <Link href="/auth/login" asChild>
-            <Button border="2px solid #C0A2E2" width={150}>
-              Войти в аккаунт
-            </Button>
-          </Link>
+              <Link href="/auth/login" asChild>
+                <Button border="2px solid #C0A2E2" width={150}>
+                  Войти в аккаунт
+                </Button>
+              </Link>
+            </>
+          ) : (
+            <>
+              <Link href="/events/create" asChild>
+                <Button backgroundColor="#E8FF59" width={200}>
+                  Создать мероприятие
+                </Button>
+              </Link>
+
+              <Button
+                border="2px solid #C0A2E2"
+                width={150}
+                onPress={handleLogout}
+              >
+                Выйти
+              </Button>
+            </>
+          )}
         </View>
       </Animated.View>
     </Animated.View>
